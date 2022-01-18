6b0f5e795827b43160544cf9c40ab8c6896f88b7
leens witts <leenswitts@gmail.com>
2022-01-18 21:43:11 +0530

    jumbo

--- src/components/jumbo/styles/jumbo.js
+++ src/components/jumbo/styles/jumbo.js
@@ -18,6 +18,24 @@ export const Container = styled.div``;
 export const Pane = styled.div`
   width: 50%;
 `;
-export const Title = styled.h1``;
-export const SubTitle = styled.h2``;
-export const Image = styled.image``;
+export const Title = styled.h1`
+  font-size: 50px;
+  font-weight: bold;
+  line-height: 1.1;
+  margin-bottom: 8px;
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+`;
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
